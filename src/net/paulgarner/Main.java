7b23671ac4791158a90765dd599e592dede0126d
Paul Garner <p.j.garner@gmail.com>
2020-11-16 10:59:05 +0000

    refactoring master

--- src/net/paulgarner/Main.java
+++ src/net/paulgarner/Main.java
@@ -8,10 +8,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String test = "Hello";
-
-        System.out.println(test.length() + Math.PI);
-
         List<Integer> series = new ArrayList<>();
 
         // 1 1 2 3 5
